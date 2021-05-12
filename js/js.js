5a3a27b5399aefd1370df96c46e0ba530147bad6
weiluge <topy@vip.qq.com>
2021-05-12 19:54:32 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -144,7 +144,7 @@ $('.Select-box-2 li').click(function () {
 		_tihs = 'https://www.bing.com/search';
 		_name = 'q';
 	} else if (_tihs == 'miji_s') {
-		_tihs = 'https://www.dogedoge.com/results';
+		_tihs = 'https://so.toutiao.com/search;
 		_name = 'q';
 	} else {
 		_tihs = 'https://www.baidu.com/s';
